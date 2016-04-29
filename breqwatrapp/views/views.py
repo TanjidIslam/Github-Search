a1d98aab7381d7f5dc0f8a6dac8dfc9a473b4d6b
TanjidIslam <tanjid.islam@mail.utoronto.ca>
2016-04-29 15:34:21 -0400

    User is shown from the search

--- breqwatrapp/views/views.py
+++ breqwatrapp/views/views.py
@@ -1,7 +1,24 @@
+from flask import render_template, session, redirect, request, url_for
+from requests import get
 from breqwatrapp import app
-from flask import render_template
 
 
 @app.route('/')
 def main_page():
     return render_template("mainSearch.html")
+
+
+@app.route('/', methods=['POST'])
+def search_query():
+    session["username"] = request.form["username"]
+    return redirect(url_for('search_result'))
+
+
+@app.route('/results')
+def search_result():
+    url = "https://api.github.com/users"
+    search = "/" + session["username"]
+    user = get(url + search).json()
+    print(user)
+    repo = get(user["repos_url"]).json()
+    return render_template('searchResult.html', user=user, repo=repo)
